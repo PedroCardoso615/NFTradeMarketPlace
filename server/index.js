052933f4c3eaf670a91734d805d1f8247b943abb
Pedro Cardoso <pedroflfc@gmail.com>
2025-03-13 12:27:57 +0000

    vercel origin

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const checkDailyRewards = require("./utils/dailyRewardCheck");
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://nf-trade-market-place-tk6n.vercel.app",
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
