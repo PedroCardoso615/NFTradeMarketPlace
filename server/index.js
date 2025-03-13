521235b385e19c571aeabcc58dd79c7c8e80c791
Pedro Cardoso <pedroflfc@gmail.com>
2025-03-13 12:44:04 +0000

    vercel error debug try

--- server/index.js
+++ server/index.js
@@ -43,6 +43,11 @@ const startUpServer = async () => {
 
   checkDailyRewards();
 
+  app.get("/", (req, res) => {
+    res.send("Hello, World!");
+  });
+  
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
